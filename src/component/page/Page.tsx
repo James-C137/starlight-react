24f1def78e52e09e0cb283433656f4655511595a
James Zhao <acc.jamesz@pm.me>
2022-11-19 19:03:02 -0500

    add page overlay

--- src/component/page/Page.tsx
+++ src/component/page/Page.tsx
@@ -11,7 +11,7 @@ export const Page = ({showSidenav}: PageProps) => {
     <div className='slr-page'>
       <Sidenav open={showSidenav} />
       <main className={`slr-page-main ${showSidenav ? 'slr-page-main-sidenav-open' : 'slr-page-main-sidenav-closed'}`}>
-        <div className={`${showSidenav ? 'slr-page-main-overlay-visible' : 'slr-page-main-overlay-invisible'}`} />
+        <div className={`slr-page-main-overlay ${showSidenav ? 'slr-page-main-overlay-visible' : 'slr-page-main-overlay-invisible'}`} />
         <Navbar />
       </main>
     </div>
