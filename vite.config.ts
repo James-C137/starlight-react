24f3a5caff69838cab8927ce0937dd5ca3efde5c
James Zhao <acc.jamesz@pm.me>
2022-11-23 14:55:20 -0500

    change build ts output

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -19,5 +20,10 @@ export default defineConfig({
       }
     }
   },
-  plugins: [react()],
+  plugins: [
+    dts({
+      insertTypesEntry: true,
+    }),
+    react()
+  ],
 })
