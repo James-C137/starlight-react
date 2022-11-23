1f43159144fce503602d7515fb784a8c34bd356a
James Zhao <acc.jamesz@pm.me>
2022-11-23 14:14:44 -0500

    change build entry point

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'starlight-react',
+      fileName: 'starlight-react',
+    },
+    rollupOptions: {
+      external: ['react'],
+      output: {
+        globals: {
+          react: 'react',
+        }
+      }
+    }
+  },
+  plugins: [react()],
 })
