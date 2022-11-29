885a9593bab804f844e8b7818fcbb2da092800b5
James Zhao <acc.jamesz@pm.me>
2022-11-29 14:06:37 -0500

    add overflow to sidenav

--- src/stories/Sidenav.stories.tsx
+++ src/stories/Sidenav.stories.tsx
@@ -16,3 +16,20 @@ export const Default = Template.bind({});
 Default.args = {
   open: true,
 };
+
+export const Overflow = Template.bind({});
+Overflow.args = {
+  children: [
+    <p>1</p>,
+    <p>2</p>,
+    <p>3</p>,
+    <p>4</p>,
+    <p>5</p>,
+    <p>6</p>,
+    <p>7</p>,
+    <p>8</p>,
+    <p>9</p>,
+    <p>10</p>,
+  ],
+  open: true,
+}
